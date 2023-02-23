8b3d7253d2abd1e9c9a648e131eb533449d512ad
Dominika666 <zajacowna666@gmail.com>
2023-02-23 10:57:33 +0100

    add changes

--- script.js
+++ script.js
@@ -9,30 +9,50 @@ const calculateBtn = document.querySelector('.calculate');
 const pace = document.querySelector('.pace');
 const speed = document.querySelector('.speed');
 
+const standardDistance = document.querySelector('#standard-distance');
 
-const calculate = () => {
+
+
+const calculateYourDistance = () => {
 
     const newHours = parseFloat(hoursInput.value);
     const newMinutes = parseFloat(minutesInput.value);
     const newSeconds = parseFloat(secondsInput.value);
     const newDistance = parseFloat(distanceInput.value);
-
+    const newStandardDistance = parseFloat(standardDistance.value);
 
     const totalTime = (newHours * 60) + newMinutes + (newSeconds / 60);
 
-    const paceResult = totalTime / newDistance;
 
-    pace.textContent = Math.floor(paceResult) + (((paceResult - Math.floor(paceResult)) * 60).toFixed(2) / 100);
+    if (newStandardDistance == 0) {
+
+        const paceResult = totalTime / newDistance;
+
+        pace.textContent = Math.floor(paceResult) + (((paceResult - Math.floor(paceResult)) * 60).toFixed(2) / 100);
+
+        const speedTime = newDistance / (totalTime / 60);
+        speed.textContent = speedTime.toFixed(2);
 
 
-    const speedTime = newDistance / (totalTime / 60)
-    speed.textContent = speedTime.toFixed(2);
+    } else {
+
+        const newPaceResult = totalTime / newStandardDistance;
+        pace.textContent = Math.floor(newPaceResult) + (((newPaceResult - Math.floor(newPaceResult)) * 60).toFixed(3) / 100);
+
+        const speedTime = newStandardDistance / (totalTime / 60);
+        speed.textContent = speedTime.toFixed(3);
 
 
+    }
 
 };
 
-calculateBtn.addEventListener('click', calculate);
+
+
+
+calculateBtn.addEventListener('click', calculateYourDistance);
+
+
 
 
 
